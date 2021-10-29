d7a35a6b5ce6bc95f2ffaceea30d67659c76d3a1
gennesseaux <jocelyn@gennesseaux.fr>
2021-10-29 15:11:06 +0200

    build release config

--- tools/build.sh
+++ tools/build.sh
@@ -5,7 +5,7 @@ cd ..
 
 # Build the test project
 cmake -S "test" -B "build/test" -DGIT_INFO_CMAKE_INFO=ON
-cmake --build "build/test"
+cmake --build "build/test" --config Release
 
 # run the application
 ./build/test/test_app
